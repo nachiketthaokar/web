758ab90c4f6d60738ba486beaf3c66f26ddd1f80
nachiketthaokar <75263249+nachiketthaokar@users.noreply.github.com>
2021-12-17 15:31:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+from selenium import webdriver 
+from bs4 import BeautifulSoup
+import time 
+import csv
+import requests
+import pandas as pd
+
+start_url = "https://en.wikipedia.org/wiki/List_of_brown_dwarfs"
+page = request.get(url)
+soup = bs(page,text,'html.parser')
+star_table = soup.find_all('table')
+table_row = star_table[7].find_all('te')
+browser = webdriver.Chrome("C:/Users/user/Desktop/pycodes/Webscriping1/chromedriver_win32/chromedriver")
+browser.get(start_url)
+time.sleep(10)
+
+temp_list= []
+table_rows = star_table.find_all('tr')
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text.rstrip() for i in td]
+    temp_list.append(row)
+
+def scrape():
+    headers = []
+    planet_data =[]
+
+Star_names = []
+Distance =[]
+Mass = []
+Radius =[]
+
+for i in range(1,len(temp_list)):
+    Star_names.append(temp_list[i][0])
+    Distance.append(temp_list[i][5])
+    Mass.append(temp_list[i][7])
+    Radius.append(temp_list[i][8])
+    
+   
+
+
+df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius)),columns = ["Star_name","Distance","Mass","Radius"])         
+print(df2)
+
+df2.to_csv("draf_stars.csv")
+
